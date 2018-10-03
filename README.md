a85b71b2218dee1136e4e18328a95e8763c0641b
Mace Ojala <mace.ojala@gmail.com>
2018-10-03 23:25:23 +0200

    A template project with some information what this is about

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python interface to DMI-TCAT
+
+This is a simple module for dealing with [Digital Methods Initiative's TCAT](https://github.com/digitalmethodsinitiative/dmi-tcat).
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='dmi_tcat',
+                 version='0.1',
+                 author="Mace Ojala",
+                 author_email="mace.ojala@gmail.com",
+                 description="A little Python interface to DMI TCAT",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/xmacex/dmi_tcat",
+                 packages=setuptools.find_packages(),
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+                     "Operating System :: OS Independent",
+                     ]
+                 )
